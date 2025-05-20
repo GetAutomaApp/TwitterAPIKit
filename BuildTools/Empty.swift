47bacd3f3926ea09a609c49937db1398a34543f5
AdonisCodes <business@simonferns.com>
2025-05-20 05:19:14 -0600

    fix: remove build tools

--- BuildTools/Empty.swift
+++
@@ -1,9 +0,0 @@
-// Empty.swift
-// Copyright (c) 2025 GetAutomaApp
-// All source code and related assets are the property of GetAutomaApp.
-// All rights reserved.
-//
-// This Package is a heavily modified fork of https://github.com/mironal/TwitterAPIKit.
-// This Package is distributable through a modified version of the MIT License.
-
-import Foundation
