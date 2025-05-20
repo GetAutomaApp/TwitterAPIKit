53c47602c863a5e45f4688e7fa1f0b4198a20b90
AdonisCodes <business@simonferns.com>
2025-05-20 05:18:03 -0600

    fix

--- build_tools.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-swift build -c release --package-path BuildTools --product swift-format
--- format.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-swift run --skip-build -c release --package-path BuildTools swift-format -p --recursive ./Sources ./Tests ./scripts --in-place
