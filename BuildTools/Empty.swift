a2e3877930b1af9084ee56557c5567a1b179f2e3
AdonisCodes <business@simonferns.com>
2025-05-20 05:05:22 -0600

    fix: remove more files

--- BuildTools/Empty.swift
+++
@@ -1,6 +0,0 @@
-// Empty.swift
-// Copyright (c) 2025 GetAutomaApp
-// All source code and related assets are the property of GetAutomaApp.
-// All rights reserved.
-
-import Foundation
--- lint.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-swift run --skip-build -c release --package-path BuildTools swift-format lint -p --strict --recursive ./Sources ./Tests ./scripts
