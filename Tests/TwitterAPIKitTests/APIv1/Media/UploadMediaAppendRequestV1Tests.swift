54d2948660396cc560970606c59cfca13b775669
AdonisCodes <business@simonferns.com>
2025-06-02 05:54:40 -0600

    fix: cast always succeeds

--- Tests/TwitterAPIKitTests/APIv1/Media/UploadMediaAppendRequestV1Tests.swift
+++ Tests/TwitterAPIKitTests/APIv1/Media/UploadMediaAppendRequestV1Tests.swift
@@ -25,7 +25,7 @@ internal class UploadMediaAppendRequestV1Tests: XCTestCase {
         XCTAssertEqual(req.path, "/1.1/media/upload.json")
         XCTAssertEqual(req.bodyContentType, .multipartFormData)
         XCTAssertEqual(
-            req.parameters as? [String: MultipartFormDataPart],
+            req.parameters,
             [
                 "command": MultipartFormDataPart.value(name: "command", value: "APPEND"),
                 "media_id": MultipartFormDataPart.value(name: "media_id", value: "m"),
