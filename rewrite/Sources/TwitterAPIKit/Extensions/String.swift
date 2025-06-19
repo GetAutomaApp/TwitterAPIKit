4d36363994d3f7713745c52a32d5f70ff11110ed
AdonisCodes <business@simonferns.com>
2025-06-19 05:13:27 -0600

    fix: remove rewrite

--- rewrite/Sources/TwitterAPIKit/Extensions/String.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-internal extension String {
-    public var urlEncoded: String {
-        // swiftlint:disable:next force_unwrapping
-        self.addingPercentEncoding(withAllowedCharacters: CharacterSet(charactersIn: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.~"))!
-    }
-}
--- rewrite/Tests/TwitterAPIKitTests/TwitterAPIKitTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-@testable import TwitterAPIKit
-
-final class TwitterAPIKitTests: XCTestCase {
-    func testExample() throws {
-        XCTAssertTrue(true)
-    }
-} 
